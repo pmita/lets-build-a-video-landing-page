f9b6ef3a8847823510dbcd3d944a94e5ecfa6468
pmita <panos.mita2@outlook.com>
2021-04-23 16:48:28 +0100

    data pre loader added

--- app.js
+++ app.js
@@ -3,3 +3,22 @@
 /*------Global Variables---------*/
 const btn = document.querySelector('.switch-btn');
 const video = document.querySelector('.video-container');
+
+
+/*-----------Event Listeners------*/
+btn.addEventListener('click', ()=>{
+    if(!btn.classList.contains('slide')){
+        btn.classList.add('slide')
+        video.pause();
+    } else {
+        btn.classList.remove('slide');
+        video.play();
+    }
+});
+
+
+const preloader = document.querySelector('.preloader');
+
+window.addEventListener('load', ()=>{
+    preloader.classList.add('hide-preloader');
+});
